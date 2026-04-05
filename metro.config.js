696d60d7998d7fcec274b4692904cd719e55cb95
root <root@srv691918.hstgr.cloud>
2026-04-05 00:53:21 +0000

    Fix: metro config serializer + react-native-screens compat for EAS build

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
 var { getDefaultConfig } = require('expo/metro-config');
+var { mergeConfig } = require('@react-native/metro-config');
 
-var config = getDefaultConfig(__dirname);
+var expoConfig = getDefaultConfig(__dirname);
 
-module.exports = config;
+// Remove expo's custom serializer that causes "not valid JSON" error
+// during EAS build's export:embed step
+delete expoConfig.serializer;
+
+module.exports = mergeConfig(expoConfig, {});
